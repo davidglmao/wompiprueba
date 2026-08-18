bfceaebc230ea2706dbbbb9ebd630aae765f2a20
davidglmao <cristhian.gutierrez@cun.edu.co>
2026-08-17 22:17:26 -0500

    Update gts.py

--- src/gts.py
+++ src/gts.py
@@ -1,1 +1,40 @@
-print("Wompi prueba de ingeniero")
+import argparse
+import json
+
+import pandas as pd
+
+
+def procesar_transacciones(nm_archivo_entrada):
+    nm_transacciones_aprobadas = []
+
+    with open(nm_archivo_entrada, "r", encoding="utf-8") as nm_archivo:
+        for nm_linea in nm_archivo:
+            nm_transaccion = json.loads(nm_linea)
+
+            if nm_transaccion["status"] == "APPROVED":
+                nm_transacciones_aprobadas.append({
+                    "fecha": nm_transaccion["created_at"],
+                    "bin": nm_transaccion["payment_method_type"]["extra"]["bin"],
+                    "monto": nm_transaccion["amount_in_cents"]
+                })
+
+    nm_df_aprobadas = pd.DataFrame(nm_transacciones_aprobadas)
+
+    nm_df_aprobadas["fecha"] = pd.to_datetime(nm_df_aprobadas["fecha"])
+    nm_df_aprobadas["dia"] = nm_df_aprobadas["fecha"].dt.day
+    nm_df_aprobadas["mes"] = nm_df_aprobadas["fecha"].dt.month
+    nm_df_aprobadas["anio"] = nm_df_aprobadas["fecha"].dt.year
+
+    nm_resumen = (
+        nm_df_aprobadas
+        .groupby(
+            ["dia", "mes", "anio", "bin"],
+            as_index=False
+        )
+        .agg(
+            cantidad_transacciones=("monto", "count"),
+            monto_total_aprobado=("monto", "sum")
+        )
+    )
+
+    return nm_resumen
